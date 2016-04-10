67b89139ee1ca1bf7d1917c024f462137268b50c
thetylerace13 <tylergemora@yahoo.com>
2016-04-09 21:53:40 -0700

    Added enemy types

---
+++ 2D_Tobii_Base_Project/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class MenuScript : MonoBehaviour {
+    public int state;
+    public GameObject playButton;
+    public GameObject playCollider;
+    public GameObject continueButton;
+    public GameObject continueCollider;
+
+	// Use this for initialization
+	void Start ()
+    {
+        state = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (state == 1)
+        {
+            playButton.SetActive(true);
+            playCollider.SetActive(true);
+        }
+        else
+        {
+            playButton.SetActive(false);
+            playCollider.SetActive(false);
+        }
+
+        if (state == 3)
+        {
+            continueButton.SetActive(true);
+            continueCollider.SetActive(true);
+        }
+        else
+        {
+            continueButton.SetActive(false);
+            continueCollider.SetActive(false);
+        }
+
+        if (playCollider.GetComponent<Button>().isPressed)
+        {
+            switchState(2);
+        }
+        if (continueCollider.GetComponent<Button>().isPressed)
+        {
+            StartCoroutine(GameObject.FindWithTag("GameController").GetComponent<Level>().LevelChange(true));
+            switchState(1);
+        }
+    }
+
+    void switchState(int newState)
+    {
+        state = newState;
+    }
+}
